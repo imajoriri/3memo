963a4447f819c5d5f73dba419eaa8d01ec908628
imajoriri <imajo.srroni@gmail.com>
2020-03-09 16:05:19 +0900

    Initial Commit

---
+++ 3m/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  3m
+//
+//  Created by imajo-takeyuki on 2020/03/09.
+//  Copyright © 2020 imajo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
